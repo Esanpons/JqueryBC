f7f7ec963fa7744f06814394328245de18a70a09
Esanpons <ecs200@hotmail.com>
2022-07-10 17:49:13 +0200

    .

--- src/controladdin/Jquery/js/Scripts.js
+++ src/controladdin/Jquery/js/Scripts.js
@@ -80,23 +80,6 @@ function todoElHtmlInicial() {
 
 }
 
-function HtmlJqueryUI() {
-    //creamos un titulo y lo añadimos al parrafo
-    var par1 = $("<p />", { id: "p1" });
-    var h1Par1 = $("<h1 />");
-    h1Par1.html("HTML jQuery UI");
-    par1.append(h1Par1);
-    controlAddIn.append(par1);
-
-    var br = $("<br />");
-    controlAddIn.append(br);
-
-    //me he quedado aquí:  https://www.udemy.com/course/draft/21679/learn/lecture/271345#questions
-
-}
-
-
-
 function notify(Mgs) {
     $('#div1').html('hola boton: ' + Mgs.data.value);
 }
@@ -157,3 +140,24 @@ function SetDataText(SetText) {
     console.log('SetText');
     $('#div1').html(SetText);
 }
+
+
+//*********************************************************************************************************************
+//jQuery UI
+//*********************************************************************************************************************
+
+function HtmlJqueryUI() {
+    //creamos un titulo y lo añadimos al parrafo
+    var par1 = $("<p />", { id: "p1" });
+    var h1Par1 = $("<h1 />");
+    h1Par1.html("HTML jQuery UI");
+    par1.append(h1Par1);
+    controlAddIn.append(par1);
+
+    var br = $("<br />");
+    controlAddIn.append(br);
+
+    //me he quedado aquí:  https://www.udemy.com/course/draft/21679/learn/lecture/271345#questions
+
+}
+
